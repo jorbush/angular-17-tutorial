df45207545ece5405184abd7135e350be1839efe
Jordi <55429631+jorbush@users.noreply.github.com>
2024-09-16 13:44:41 +0200

    use user component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<h1>Hello world from {{ city.toUpperCase() }}</h1>`,
+  imports: [RouterOutlet, UserComponent],
+  template: `<h1>Hello world from {{ city.toUpperCase() }}</h1>
+  <app-user/>`,
   styles: ['']
 })
 export class AppComponent {
